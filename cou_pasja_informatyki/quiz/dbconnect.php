ff8d5e2c6a9c096767ca82b47654048a8fa99c1d
Michał Sikorski <michalsikorski61@gmail.com>
2024-11-15 14:22:24 +0100

    pasja sql 1 - simple select

---
+++ cou_pasja_informatyki/quiz/dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+	$host="localhost";		// Nazwa hosta
+	$user="root"; 				// Nazwa użytkownika - domyślnie: root
+	$password=""; 			// Haslo do bazy
+	$database="quir";	 	// Nazwa bazy
+	$table="pytania" 			// Nazwa tabeli
+	
+?>
+
